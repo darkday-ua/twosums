2364046dde2f08888b03bcf6d58eb547a9a5717f
Ivanko Pavel <ivanko.pavel@gmail.com>
2021-09-01 14:29:28 +0300

    added two_sum_bst

---
+++ two_sum_IV_bst.py
@@ -0,0 +1,61 @@
+"""
+653. Two Sum IV - Input is a BST
+Easy
+
+Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+ 
+
+Example 1:
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+
+Example 3:
+
+Input: root = [2,1,3], k = 4
+Output: true
+
+Example 4:
+
+Input: root = [2,1,3], k = 1
+Output: false
+
+Example 5:
+
+Input: root = [2,1,3], k = 3
+Output: true
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 104].
+    -104 <= Node.val <= 104
+    root is guaranteed to be a valid binary search tree.
+    -105 <= k <= 105
+
+"""
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        return True
+
+
+sol = Solution()
+print(sol.findTarget(root=[5, 3, 6, 2, 4, None, 7], k=28))
